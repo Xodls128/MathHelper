03b5819aae9a69cd792ff8c1209c2916c8a29165
xodls <pider6@naver.com>
2025-07-29 19:08:44 +0900

    FEAT: 풀이앱 모델 구현

--- solutions/models.py
+++ solutions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from problems.models import Problem
 
-# Create your models here.
+class Solutions(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    
+    #사용자 입력
+    content = models.ImageField(upload_to='solution_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    #AI 처리 결과 (MCP 구현 후 적용예정)
+    # ocr_text = models.TextField(null=True, blank=True)
+    # logic_steps = models.JSONField(null=True, blank=True)
+    # feedback_text = models.TextField(null=True, blank=True)
+    # mistake_spot = models.JSONField(null=True, blank=True)
+    # score = models.IntegerField(null=True, blank=True)
