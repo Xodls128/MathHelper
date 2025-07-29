36aa74fada4acbf103b6b67473ac878ceedcaadc
xodls <pider6@naver.com>
2025-07-29 20:30:25 +0900

    FEAT: 문제 뷰 구현 (문제 목록, 문제 조회, 문제 생성)

--- problems/views.py
+++ problems/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from problems.models import Problem
+from problems.serializers.problem import ProblemSerializer
 
-# Create your views here.
+class ProblemListView(generics.ListAPIView):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+class ProblemRetrieveView(generics.RetrieveAPIView):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+
+class ProblemCreateView(generics.CreateAPIView):
+    queryset = Problem.objects.all()
+    serializer_class = ProblemSerializer
+    permission_classes = [permissions.IsAdminUser]
+
