de850cad808d1f93539f9d63ba4aaff62b7bbe97
xodls <pider6@naver.com>
2025-07-29 20:32:03 +0900

    FEAT: 풀이 뷰 구현 (풀이 제출, 내가 제출한 풀이 목록, 특정문제에 대한 풀이목록)

--- solutions/models.py
+++ solutions/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from problems.models import Problem
 
-class Solutions(models.Model):
+class Solution(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     
--- solutions/views.py
+++ solutions/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from solutions.models import Solution
+from solutions.serializers.solution import SolutionSerializer
 
-# Create your views here.
+class SolutionCreateView(generics.CreateAPIView):
+    serializer_class = SolutionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class MySolutionListView(generics.ListAPIView):
+    serializer_class = SolutionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Solution.objects.filter(user=self.request.user)
+
+class ProblemSolutionListView(generics.ListAPIView):
+    serializer_class = SolutionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        problem_id = self.kwargs['problem_id']
+        return Solution.objects.filter(problem__id=problem_id)
