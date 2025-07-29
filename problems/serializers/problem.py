468f2d77ead918d9347847c3e9db5e351c5113ce
xodls <pider6@naver.com>
2025-07-29 20:23:31 +0900

    FEAT: 문제 앱 problem 시리얼라이저 구현

---
+++ problems/serializers/problem.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from problems.models import Problem
+
+class ProblemSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(required=False)
+
+    class Meta:
+        model = Problem
+        fields = ['id', 'title', 'image', 'difficulty', 'tags', 'created_at']
