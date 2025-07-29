035ef724e2641b9e37abd797d59f161db3983ea0
xodls <pider6@naver.com>
2025-07-29 19:04:08 +0900

    FEAT: 문제앱의 모델 구현

--- problems/models.py
+++ problems/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ProblemTag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Problem(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.ImageField(upload_to='problem_images/')
+    difficulty = models.IntegerField()
+    tags = models.ManyToManyField(ProblemTag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
