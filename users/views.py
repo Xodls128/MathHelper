21686511e2089ee3719be69661a93230ec476b4d
xodls <pider6@naver.com>
2025-07-29 20:29:33 +0900

    FEAT: 유저 뷰 구현(본인 정보 확인할 뷰)

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from users.models import CustomUser
+from users.serializers.user import UserSerializer
 
-# Create your views here.
+class UserMeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
