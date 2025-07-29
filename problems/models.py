93a6c9112f2249bb0cb21b3465015853895329a3
xodls <pider6@naver.com>
2025-07-29 20:37:59 +0900

    DEAL: 문제 앱의 tag삭제(모델, 시리얼라이저 삭제) : 굳이 지금 필요하지않기때문

--- problems/models.py
+++ problems/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-class ProblemTag(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
 class Problem(models.Model):
     title = models.CharField(max_length=200)
     content = models.ImageField(upload_to='problem_images/')
--- problems/serializers/tag.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from problems.models import ProblemTag
-
-class ProblemTagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProblemTag
-        fields = ['id', 'name']
